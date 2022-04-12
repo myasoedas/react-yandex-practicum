eb6dadc6434ba028269105098883d763a07bbf11
Александ Мясоед @alexproit Telegram <79302838+myasoedas@users.noreply.github.com>
2022-04-12 15:20:42 +0300

    Add files via upload

---
+++ task.jsx
@@ -0,0 +1,53 @@
+class Card extends React.Component {
+  render() {
+    const data = {
+      id: 120323,
+      name: 'Герметичные валенки с магнитной подошвой',
+      priceInfo: {
+        price: 29224,
+        currency: 'руб',
+        taxes: 20
+      },
+      properties: {
+        color: 'серый',
+        weight: 2.4,
+        material: 'шерсть'
+      },
+      info: {
+        fullDecription: `Валенки, в которых не страшно передвигаться даже снаружи космической станции.
+          Магнитная подошва позволит без страха шагать по поверхности корабля, а многослойный
+          материал StarTex удерживает тепло внутри обуви даже в открытом космосе.`
+      },
+      category: 'Обувь',
+      gender: 'Унисекс'
+    };
+
+    return (
+      <article className="main">
+        <h1 className="main__title">{data.name}</h1>
+        <div className="main__container">
+          <p className="main__text">{`Цена: ${data.priceInfo.price} ${
+            data.priceInfo.currency
+          }.`}</p>
+          <p className="main__text">{`Налог: ${data.priceInfo.taxes}%`}</p>
+        </div>
+        <div className="main__container">
+          <h3 className="main__subtitle">Характеристики:</h3>
+          <p className="main__text">{`Цвет: ${data.properties.color}`}</p>
+          <p className="main__text">{`Материал: ${data.properties.material}`}</p>
+          <p className="main__text">{`Вес: ${data.properties.weight} кг`}</p>
+        </div>
+        <div className="main__container">
+          <h3 className="main__subtitle">Описание:</h3>
+          <p className="main__text">{data.info.fullDecription}</p>
+        </div>
+        <div className="main__tags">
+          <p className="main__tag">{data.category}</p>
+          <p className="main__tag">{data.gender}</p>
+        </div>
+      </article>
+    );
+  }
+}
+
+ReactDOM.render(<Card />, document.querySelector('#root'));

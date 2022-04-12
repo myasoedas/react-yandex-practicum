0b740bb71a1010a362239a2dd36eed03aeea5a5b
Александ Мясоед @alexproit Telegram <79302838+myasoedas@users.noreply.github.com>
2022-04-12 11:21:03 +0300

    Update task.jsx

--- task.jsx
+++ task.jsx
@@ -3,16 +3,16 @@ class GoodNews extends React.Component {
     return (
       <div className="content">
         <h1 className="content__title">Лента хороших новостей</h1>
-        <div id="news-container">
-          <section>Уникальный зебрёнок в крапинку родился в Кении</section>
-          <section>
+        <div id="news-container" className="beautiful-container d-flex-column">
+          <section className="post good-news">Уникальный зебрёнок в крапинку родился в Кении</section>
+          <section className="post good-news">
             Туристы обнаружили выживавшую в снегах две недели собаку и вернули ее домой
           </section>
-          <section>На Аляске выбрали самого жирного медведя</section>
-          <section>
+          <section className="post good-news">На Аляске выбрали самого жирного медведя</section>
+          <section className="post good-news">
             Хитрый чихуахуа приучился кататься верхом и плавать на своем друге лабрадоре
           </section>
-          <section>Шимпанзе бонобо любят поговорить о еде</section>
+          <section className="post good-news">Шимпанзе бонобо любят поговорить о еде</section>
         </div>
       </div>
     );

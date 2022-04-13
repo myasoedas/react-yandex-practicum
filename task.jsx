e514edb70d9c6d626ec81043e04eeac0190a18ce
Александ Мясоед @alexproit Telegram <79302838+myasoedas@users.noreply.github.com>
2022-04-13 21:06:52 +0300

    Add files via upload

---
+++ task.jsx
@@ -0,0 +1,34 @@
+class SettingsMenu extends React.Component {
+  state = {
+    theme: 'dark',
+    userSettings: {
+      notificationsEnabled: true,
+      sidebar: {
+        title: 'Боковая панель',
+        enabled: false
+      }
+    }
+  };
+
+  toggleSidebar = () => {
+    this.setState(prevState => ({
+      /* Ваш код будет здесь */
+    }));
+  };
+
+  render() {
+    const { enabled } = this.state.userSettings.sidebar;
+    return (
+      <>
+        {enabled && <aside className="aside" />}
+        <section className="content">
+          <button className="content__button" onClick={this.toggleSidebar}>
+            {enabled ? 'Выключить' : 'Включить'}
+          </button>
+        </section>
+      </>
+    );
+  }
+}
+
+ReactDOM.render(<SettingsMenu />, document.querySelector('#root'));

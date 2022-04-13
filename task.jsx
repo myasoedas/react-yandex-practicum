6f3cab763a589ae3f5b44d0fd6958ddacfbcb9d2
Александ Мясоед @alexproit Telegram <79302838+myasoedas@users.noreply.github.com>
2022-04-13 11:18:31 +0300

    feat: Решение 1

--- task.jsx
+++ task.jsx
@@ -2,26 +2,25 @@ const MenuBlock = props => {
   return (
     <div className="menu">
       <div className="content">
-        <p className="menu__title">{props.children}</p>
-        {props.menuItems}
+        <p className="menu__title">{props.menuHeading}</p>
+        {props.children}
       </div>
     </div>
   );
 };
 
-const Footer = () => {
-  const menuItems = (
-    <ul className="nav">
-      <li className="nav__link">Наша миссия</li>
-      <li className="nav__link">Вакансии</li>
-      <li className="nav__link">Блог</li>
-    </ul>
-  );
+const Footer = () => {  
   return (
     <div className="footer">
       <div className="container">
         <img className="menu__img" src="./images/bg.svg" alt="Подвал" />
-        <MenuBlock menuItems={menuItems}>О компании</MenuBlock>
+        <MenuBlock menuHeading="О компании">
+          <ul className="nav">
+            <li className="nav__link">Наша миссия</li>
+            <li className="nav__link">Вакансии</li>
+            <li className="nav__link">Блог</li>
+          </ul>
+        </MenuBlock>
       </div>
     </div>
   );

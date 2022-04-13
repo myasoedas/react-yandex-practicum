baad02e009e5117deeb02f09a4a5ce731072b735
Александ Мясоед @alexproit Telegram <79302838+myasoedas@users.noreply.github.com>
2022-04-13 10:10:56 +0300

    feat: Решение 1
    <p className='text'>{props.children}</p> 
    <Quote author="Принцесса Ирулан «Избранные&nbsp;изречения Муад'Диба»">
    Арракис учит «пониманию ножа», учит относиться к жизни так: отрезать всё несовершенное и незавершённое, говоря: «Вот теперь это совершенно и завершено — ибо кончается здесь».
    </Quote>

--- task.jsx
+++ task.jsx
@@ -1,7 +1,7 @@
 const Quote = (props) => {
   return (
     <div className="fancy-quote">
-      <p className='text'>{props.quoteContent}</p> {/* Переписать на props.children */}
+      <p className='text'>{props.children}</p> 
       <span className='author'>{props.author}</span>
     </div>
   );
@@ -10,10 +10,9 @@ const Quote = (props) => {
 
 const DuneQuote = () => {
   return ( 
-    <Quote
-      author="Принцесса Ирулан «Избранные&nbsp;изречения Муад'Диба»"
-      quoteContent="Арракис учит «пониманию ножа», учит относиться к жизни так: отрезать всё несовершенное и незавершённое, говоря: «Вот теперь это совершенно и завершено — ибо кончается здесь»."
-    />
+    <Quote author="Принцесса Ирулан «Избранные&nbsp;изречения Муад'Диба»">
+Арракис учит «пониманию ножа», учит относиться к жизни так: отрезать всё несовершенное и незавершённое, говоря: «Вот теперь это совершенно и завершено — ибо кончается здесь».
+    </Quote>
   );
 }
 

185bdac8ae86a1117d2100d22797ee6bd5f1628e
Александ Мясоед @alexproit Telegram <79302838+myasoedas@users.noreply.github.com>
2022-04-13 20:34:07 +0300

    feat: Решение 1

--- task.jsx
+++ task.jsx
@@ -1,11 +1,18 @@
 class Door extends React.Component {
   state = { isOpen: false };
+  addKey = () => {
+    this.setState({ isOpen: !this.state.isOpen })
+  }
 
   render() {
     return (
       <div className={this.state.isOpen ? 'content bg_open' : 'content bg_close'}>
-        <h1 className="content__title" />
-        <button className="content__btn">{this.state.isOpen ? 'Закрыть' : 'Открыть'}</button>
+        <h1 className="content__title">
+          {this.state.isOpen ? "Замок открыт 🔓" : "Замок закрыт 🔒"}
+        </h1>
+        <button onClick={this.addKey} className="content__btn">
+          {this.state.isOpen ? 'Закрыть' : 'Открыть'}
+        </button>
       </div>
     );
   }

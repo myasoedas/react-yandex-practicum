7b7e63ee7efde1942a766fa370830ddf502681d1
Александ Мясоед @alexproit Telegram <79302838+myasoedas@users.noreply.github.com>
2022-04-14 16:11:42 +0300

    Add files via upload

---
+++ task.jsx
@@ -0,0 +1,31 @@
+class Image extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { fullScreen: false };
+  }
+
+  toggleFullScreen() {
+    this.setState({ fullScreen: !this.state.fullScreen });
+  }
+
+  render() {
+    const { fullScreen } = this.state;
+    const className = fullScreen ? 'image image-fullscreen' : 'image';
+    const buttonClassName = fullScreen ? 'btn btn-close' : 'btn btn-open';
+    const style = {
+      backgroundImage: `url(${this.props.src})`
+    };
+    return (
+      <div className={className} style={style}>
+        <button className={buttonClassName} onClick={this.toggleFullScreen} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <div className="main">
+    <Image src="./images/bg.png" />
+  </div>,
+  document.querySelector('#root')
+);

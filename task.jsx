2b3e5336777946b0c987afd52ac62394922c916c
Александ Мясоед @alexproit Telegram <79302838+myasoedas@users.noreply.github.com>
2022-04-15 09:27:35 +0300

    Add files via upload

---
+++ task.jsx
@@ -0,0 +1,30 @@
+class MouseTrack extends React.Component {
+  state = {
+    mousePosition: []
+  };
+  
+  componentDidMount() {
+    document.addEventListener('mousemove', (e) => this.setState({mousePosition: [e.clientX, e.clientY]}))
+  };
+  
+  trackMousePos = e => {
+    this.setState({
+      mousePosition: [e.clientX, e.clientY]
+    })
+  }
+  
+  
+
+  render() {
+    return (
+      <div>
+        <h3 className="app__title">Позиция курсора:</h3>
+        <h2>
+          X: {this.props.trackMouse ? this.state.mousePosition[0] : ' -'}
+          <br />
+          Y: {this.props.trackMouse ? this.state.mousePosition[1] : ' -'}
+        </h2>
+      </div>
+    );
+  }
+}
